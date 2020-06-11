83450870bfb61ad3fca1b520a9e2d9b2dac8bd6a
abelrufino <anetosib@hotmail.com>
2020-06-11 01:09:45 -0300

    Projetos Java Eclipse update 04

---
+++ SetTestaIgualdade/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Set<Product> set = new HashSet<Product>();
+
+		set.add(new Product("TV", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));
+
+		Product prod = new Product("Notebook", 1200.0);
+
+		System.out.println(set.contains(prod));
+
+	}
+
+}
