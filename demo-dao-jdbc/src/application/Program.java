f20c9b30ea8810092c747b4c372dd9dc63901af2
abelrufino <anetosib@hotmail.com>
2020-07-14 01:44:08 -0300

    findById implementation

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,10 +1,7 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,11 +9,12 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(1, "Abel", "anetosib@gmail.com", new Date(), 1700.0, obj);
+	//	Department obj = new Department(1, "Books");
+	//	Seller seller = new Seller(1, "Abel", "anetosib@gmail.com", new Date(), 1700.0, obj);
 	//	System.out.println(obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
 		
